be2dcc3a885dc2be966c3b83d8a1949032d36858
Brage <brage.spammail@gmail.com>
2021-08-17 11:21:29 +0200

    chore: add future constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,3 +1,45 @@
 import { IntentsString } from "discord.js";
 
 export const INTENTS: IntentsString[] = ["GUILDS"];
+
+// * REGEXP
+// export const CODEBLOCK_REGEX = /```(?:(?<lang>\S+)\n)?\s?(?<code>[^]+?)\s?```/;
+// export const TOKEN_REGEX = /[\w-]{24}\.[\w-]{6}\.[\w-]{27}/;
+// export const INVITE_REGEX = /(?:https?:\/\/)?(?:www\.)?discord(?:\.gg|(?:app)?\.com\/invite)\/(\S+)/;
+
+// export const CHANNEL_REGEX = /^<#\d{17,19}>$/;
+// export const ROLE_REGEX = /^<@&\d{17,19}>$/;
+// export const USER_REGEX = /^<@!?\d{17,19}>$/;
+
+// export const ID_REGEX = /^\d{17,19}$/;
+// *
+
+// * COLORS
+// export const BLURPLE = "5865F2";
+// export const ORANGE = "FF8741";
+// export const YELLOW = "FFC152";
+// export const BLACK = "000000";
+// export const GREEN = "5AD658";
+// export const INVIS = "2F3136";
+// export const WHITE = "FFFFFF";
+// export const RED = "FF5733";
+// *
+
+// * PHRASES
+/*export const JOIN_PHRASES = [
+	"Nice to see you, ¤!",
+	"Much welcome, ¤!",
+	"Good morning, ¤!",
+	"What's up, ¤?",
+	"Hello, ¤!",
+	"Hiii, ¤!"
+];*/
+/*export const LEAVE_PHRASES = [
+	"Hope too see you soon, ¤!",
+	"Until next time, ¤!",
+	"See you later, ¤!",
+	"Farewell, ¤!",
+	"Bye bye, ¤!",
+	"Adiós, ¤!"
+];*/
+// *
