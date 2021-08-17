6cc5311470d6ab3a6cf6e77a656598b470491d7a
Brage <brage.spammail@gmail.com>
2021-08-17 11:03:10 +0200

    feat(Ping): initial command

---
+++ src/commands/util/ping.ts
@@ -0,0 +1,11 @@
+import { ApplicationCommandData } from "discord.js";
+import { CmdIntr } from "../../Typings";
+
+export const data: ApplicationCommandData = {
+    name: "ping",
+    description:"Check that the bot is running"
+}
+
+export async function execute(intr: CmdIntr) {
+    intr.editReply("piong");
+}
