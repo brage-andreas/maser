d764a72315c8153304707e56cdb8a79b3289ef0b
hellobafu <brage.spammail@gmail.com>
2021-08-18 23:07:48 +0200

    chore: logs

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,12 +5,13 @@ export async function execute(client: Clint) {
 	const time = Util.Now();
 	const guilds = client.guilds.cache.size;
 	const channels = client.channels.cache.size;
+	const users = client.guilds.cache.reduce((count, guild) => count + guild.memberCount, 0);
 
 	Util.Log(`[${time}] Started`);
 
-	if (client.user)
-		Util.Log(`Logged on as ${client.user.tag} (${client.user.id})`);
-	Util.Log(`In ${guilds} guilds and ${channels} channels`);
+	if (client.user) Util.Log(`Logged on as ${client.user.tag} (${client.user.id})`);
+	Util.Log(`In ${guilds} guilds, ${channels} channels, and ${users} users`);
+	console.log("\n");
 
 	if (!client.application?.name) await client.application?.fetch();
 }
