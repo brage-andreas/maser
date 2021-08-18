b85d4b876a7c0e3bb1b0de5918b64838ed7773a6
hellobafu <brage.spammail@gmail.com>
2021-08-18 23:07:26 +0200

    feat: clear commands

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -5,13 +5,24 @@ export async function execute(client: Clint, msg: Message) {
 	if (msg.author.id !== client.application?.owner?.id || !client.user) return;
 	if (msg.channel.type === "DM" || !msg.guild) return;
 
-	if (msg.content.startsWith("?build")) {
-		const type = msg.content.split(/\s+/g)[1];
+	const [command, argument] = msg.content.split(/\s+/g).slice(0, 2);
+	if (!command) return;
 
-		if (!type || !["client", "guild"].includes(type.toLowerCase()))
-			return msg.reply(`Unknown type: ${type ?? "No type provided"}\nMust be one of "client", "guild"`);
+	if (command === "?build") {
+		if (!argument || !["client", "guild"].includes(argument.toLowerCase()))
+			return msg.reply(`Unknown type: ${argument ?? "No type provided"}\nMust be one of "client", "guild"`);
 
 		msg.delete();
-		client.commands.put(client.user.id, type === "guild" ? msg.guild.id : undefined);
+		client.commands.put(client.user.id, argument === "guild" ? msg.guild.id : undefined);
+		return;
+	}
+
+	if (command === "?clear") {
+		if (!argument || !["client", "guild"].includes(argument.toLowerCase()))
+			return msg.reply(`Unknown type: ${argument ?? "No type provided"}\nMust be one of "client", "guild"`);
+
+		msg.delete();
+		client.commands.clear(client.user.id, argument === "guild" ? msg.guild.id : undefined);
+		return;
 	}
 }
