fe4ba54a848e0146bf4f5c9303fd6667e6c9a67a
hellobafu <brage.spammail@gmail.com>
2021-08-22 23:42:35 +0200

    feat: mention prefix and eval command

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,14 +1,25 @@
 import { Message } from "discord.js";
+import { CODEBLOCK_REGEX, CODE_REGEX, ID_REGEX, USER_REGEX } from "../Constants.js";
 import { Clint } from "../extensions/Clint.js";
+import { evaluate } from "../utils/Eval.js";
 
 export async function execute(client: Clint, msg: Message) {
 	if (msg.author.id !== client.application?.owner?.id || !client.user) return;
 	if (msg.channel.type === "DM" || !msg.guild) return;
 
-	const [command, argument] = msg.content.split(/\s+/g).slice(0, 2);
-	if (!command) return;
+	const split = msg.content.split(/\s+/g);
+	const [mention, command] = split.splice(0, 2);
+	if (!mention || !command) return;
 
-	if (command === "?build") {
+	const argument = split[0];
+
+	const isId = ID_REGEX.test(mention);
+	const isMention = USER_REGEX.test(mention);
+	const cleanId = mention.replaceAll(/\D/g, "");
+
+	if (!(isId || isMention) && cleanId === client.user.id) return;
+
+	if (command === "build") {
 		if (!argument || !["client", "guild"].includes(argument.toLowerCase()))
 			return msg.reply(`Unknown type: ${argument ?? "No type provided"}\nMust be one of "client", "guild"`);
 
@@ -17,12 +28,22 @@ export async function execute(client: Clint, msg: Message) {
 		return;
 	}
 
-	if (command === "?clear") {
+	if (command === "clear") {
 		if (!argument || !["client", "guild"].includes(argument.toLowerCase()))
 			return msg.reply(`Unknown type: ${argument ?? "No type provided"}\nMust be one of "client", "guild"`);
 
 		msg.delete();
 		client.commands.clear(client.user.id, argument === "guild" ? msg.guild.id : undefined);
+		return;
+	}
+
+	if (command === "eval") {
+		const captured = (msg.content.match(CODEBLOCK_REGEX) ?? msg.content.match(CODE_REGEX))?.groups;
+		const code = captured?.code ?? split.join(" ");
+
+		const { embeds, files } = await evaluate(msg, code);
+
+		msg.reply({ embeds, files });
 		return;
 	}
 }
