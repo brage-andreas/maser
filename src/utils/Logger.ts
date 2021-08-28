0d57a277b647b175524adb9a175fd0aa28720ceb
hellobafu <brage.spammail@gmail.com>
2021-08-28 21:40:18 +0200

    feat: cache manager and logger

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,18 +1,14 @@
-import type { CmdIntr } from "../Typings";
-
-import Util from ".";
-import { Clint } from "../extensions/Clint";
+import type { Guild, GuildChannel, User } from "discord.js";
+import Util from "./index.js";
 
 interface Cache {
-	channelId?: string;
 	channel?: string;
-	guildId?: string;
 	userId?: string;
 	guild?: string;
 	user?: string;
 }
 
-class CacheManager extends null {
+class CacheManager {
 	private _cache: Cache = this._emptyCache();
 
 	public get() {
@@ -21,6 +17,26 @@ class CacheManager extends null {
 
 	public set(newCache: Cache) {
 		this._cache = newCache;
+	}
+
+	public has(key: "USER" | "GUILD" | "CHANNEL") {
+		if (key === "CHANNEL") return !!this._cache.channel;
+		if (key === "GUILD") return !!this._cache.guild;
+		if (key === "USER") return !!this._cache.userId;
+		return false;
+	}
+
+	public setChannel(channel: GuildChannel) {
+		this._cache.channel = channel.name;
+	}
+
+	public setGuild(guild: Guild) {
+		this._cache.guild = guild.name;
+	}
+
+	public setUser(user: User) {
+		this._cache.userId = user.id;
+		this._cache.user = user.tag;
 	}
 
 	public clear() {
@@ -39,42 +55,49 @@ class CacheManager extends null {
 	}
 }
 
-class BaseLogger {
+export class BaseLogger {
 	protected cache: CacheManager;
 
 	constructor() {
 		this.cache = new CacheManager();
 	}
 
-	protected print(...messages: string[]) {
-		messages.forEach((message) => void process.stdout.write(message));
+	protected print(type: string, ...messages: string[]) {
+		const base = this._addBase(type);
+		const trace = this._addTrace();
+
+		process.stdout.write(base);
+		if (trace) process.stdout.write(" > " + trace);
+		process.stdout.write("\n");
+
+		this.parse(...messages)?.forEach((message) => void console.log(message));
 	}
 
 	protected parse(...messages: string[]) {
 		if (!messages.length) return null;
-
-		messages = messages.map((message) => Util.Parse(message) as string);
-		if (messages.length === 1) return messages[0];
-		else return messages;
-	}
-}
-
-export class CommandLogger extends BaseLogger {
-	constructor(interaction: CmdIntr) {
-		super();
+		return messages.map((message) => Util.Parse(message, 4) as string);
 	}
 
-	public log(...messages: string[]) {
-		//
-	}
-}
-
-export class EventLogger extends BaseLogger {
-	constructor(client: Clint) {
-		super();
+	private _addBase(type: string /* | "INFO" */) {
+		return `  [${type}] ${Util.Now()}`;
 	}
 
-    public log(...messages: string[]) {
-		//
+	private _addTrace() {
+		const cache = this.cache.get();
+		let trace = [];
+
+		if (this.cache.has("USER")) {
+			trace.push(cache.user ? `${cache.user} (u: ${cache.userId})` : `u: ${cache.userId}`);
+		}
+
+		if (this.cache.has("CHANNEL")) {
+			trace.push(`in #${cache.channel}`);
+		}
+
+		if (this.cache.has("GUILD")) {
+			trace.push(`in ${cache.guild}`);
+		}
+
+		return trace.join(" ");
 	}
 }
