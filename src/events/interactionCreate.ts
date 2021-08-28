6952f7ed8b70605a935219e18e8b1982776b546c
hellobafu <brage.spammail@gmail.com>
2021-08-28 21:36:50 +0200

    feat: logger

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,8 +1,10 @@
-import { Clint } from "../extensions/Clint.js";
-import { CmdIntr } from "../Typings.js";
+import type { CmdIntr } from "../Typings.js";
+import type { Clint } from "../extensions/Clint.js";
+import { CommandLogger } from "../utils/CommandLogger.js";
 
 export async function execute(client: Clint, intr: CmdIntr) {
 	if (!intr.isCommand()) return;
 
+	intr.logger = new CommandLogger(intr);
 	client.commands.execute(intr);
 }
