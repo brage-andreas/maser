ec8f325bd9bce17a777a9562d67d291522f249a5
hellobafu <brage.spammail@gmail.com>
2021-08-30 19:38:57 +0200

    refactor: moved loggers to folder

---
+++ src/utils/logger/TraceValueManager.ts
@@ -0,0 +1,55 @@
+import type { Guild, TextBasedChannels, User } from "discord.js";
+
+interface Cache {
+	channel: string | null;
+	userId: string | null;
+	guild: string | null;
+	user: string | null;
+}
+
+export class TraceValueManager {
+	private _cache: Cache = this._emptyCache();
+
+	public get() {
+		return this._cache;
+	}
+
+	public set(newCache: Cache) {
+		this._cache = newCache;
+	}
+
+	public has(key: "USER" | "GUILD" | "CHANNEL") {
+		if (key === "CHANNEL") return !!this._cache.channel;
+		if (key === "GUILD") return !!this._cache.guild;
+		if (key === "USER") return !!this._cache.userId;
+		return false;
+	}
+
+	public setChannel(channel: TextBasedChannels | null) {
+		if (!channel || channel.type !== "DM") {
+			this._cache.channel = channel?.name ?? null;
+		}
+	}
+
+	public setGuild(guild: Guild | null) {
+		this._cache.guild = guild?.name ?? null;
+	}
+
+	public setUser(user: User | null) {
+		this._cache.userId = user?.id ?? null;
+		this._cache.user = user?.tag ?? null;
+	}
+
+	public clear() {
+		this._cache = this._emptyCache();
+	}
+
+	private _emptyCache() {
+		return {
+			channel: null,
+			userId: null,
+			guild: null,
+			user: null
+		} as Cache;
+	}
+}
