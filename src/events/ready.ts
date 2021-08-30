1c0130c9eba09afcadfeb2c6a585f60178518add
hellobafu <brage.spammail@gmail.com>
2021-08-30 19:37:20 +0200

    refactor: use info logger

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,17 +1,14 @@
 import type { Clint } from "../extensions/Clint.js";
-import Util from "../utils/index.js";
 
 export async function execute(client: Clint) {
-	const time = Util.Now();
 	const guilds = client.guilds.cache.size;
 	const channels = client.channels.cache.size;
 	const users = client.guilds.cache.reduce((count, guild) => count + guild.memberCount, 0);
 
-	Util.Log(`[${time}] Started`);
-
-	if (client.user) Util.Log(`Logged on as ${client.user.tag} (${client.user.id})`);
-	Util.Log(`In ${guilds} guilds, ${channels} channels, and ${users} users`);
-	console.log("\n");
+	client.logger.log(
+		`Logged on as ${client.user!.tag} (${client.user!.id})`,
+		`In ${guilds} guilds, ${channels} channels, and ${users} users`
+	);
 
 	if (!client.application?.name) await client.application?.fetch();
 }
