84b5852e35959621ce814ff40803060991725f2b
hellobafu <brage.spammail@gmail.com>
2021-08-30 19:37:06 +0200

    refactor: moved loggers to folder

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,6 +1,6 @@
 import type { CmdIntr } from "../Typings.js";
 import type { Clint } from "../extensions/Clint.js";
-import { CommandLogger } from "../utils/CommandLogger.js";
+import { CommandLogger } from "../utils/logger/CommandLogger.js";
 
 export async function execute(client: Clint, intr: CmdIntr) {
 	if (!intr.isCommand()) return;
