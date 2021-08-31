4f69891e4a9f28351a8789e62bf2024da25aaed5
hellobafu <brage.spammail@gmail.com>
2021-08-31 08:47:06 +0200

    feat: guildCreate logs

---
+++ src/events/guildCreate.ts
@@ -0,0 +1,11 @@
+import type { Clint } from "../extensions/Clint";
+import type { Guild } from "discord.js";
+
+export async function execute(client: Clint, guild: Guild) {
+    client.events.logger
+        .setEvent("guild create")
+        .log(
+            `Joined ${guild.name} (${guild.id})`,
+            `with ${guild.channels.cache.size} channels and ${guild.memberCount} members`
+        );
+}
