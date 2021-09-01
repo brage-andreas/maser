7d102869d3d07b8e2916e9b0a3e5ca4fbd75eeaf
hellobafu <brage.spammail@gmail.com>
2021-09-01 19:38:19 +0200

    feat: get returns null instead of undefined

--- src/extensions/ColorManager.ts
+++ src/extensions/ColorManager.ts
@@ -1,22 +1,37 @@
 import { COLORS } from "../Constants.js";
 
+/**
+ * Manages colours for the client.
+ */
 export class ColorManager {
 	static HEX_REGEX = /^#[a-f0-9]{6}$/i;
 	public colors: Map<string, `#${string}`>;
 
+	/**
+	 * Creates a colour manager.
+	 */
 	constructor() {
 		this.colors = this._get();
 	}
 
-	public get(query: string) {
-		return this.colors.get(query.toUpperCase());
+	/**
+	 * Gets a colour from the manager.
+	 */
+	public get(query: string): `#${string}` | null {
+		return this.colors.get(query.toUpperCase()) ?? null;
 	}
 
-	public try(query: string) {
+	/**
+	 * Tries to get a colour from the manager. Returns black if not found.
+	 */
+	public try(query: string): `#${string}` {
 		return this.colors.get(query) ?? "#000000";
 	}
 
-	public toArray() {
+	/**
+	 * Returns an array of all colours in the manager.
+	 */
+	public toArray(): `#${string}`[] {
 		const colorArray: `#${string}`[] = [];
 		for (const [, color] of this.colors) {
 			colorArray.push(color);
@@ -24,11 +39,18 @@ export class ColorManager {
 		return colorArray;
 	}
 
-	public isValid(color: string) {
+	/**
+	 * Tests if a given string is a valid colour.
+	 */
+	public isValid(color: string): boolean {
 		color = color.trim().replace("#", "");
 		return ColorManager.HEX_REGEX.test(`#${color}`);
 	}
 
+	/**
+	 * Returns all colours from CONSTANTS in a map.
+	 * Throws an error if at least one colour is invalid.
+	 */
 	private _get() {
 		const colorMap: Map<string, `#${string}`> = new Map();
 		for (const [name, color] of Object.entries(COLORS)) {
