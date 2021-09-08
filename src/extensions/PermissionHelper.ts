31bf771da5c6b29c93f31e1bf608d51980062ec8
hellobafu <brage.spammail@gmail.com>
2021-09-08 22:58:40 +0200

    feat: unname class

--- src/extensions/PermissionHelper.ts
+++ src/extensions/PermissionHelper.ts
@@ -1,6 +1,6 @@
 import type { Channel, GuildChannel, GuildMember, TextBasedChannels } from "discord.js";
 
-export default abstract class PermissionHelper {
+export default abstract class {
 	static sendable(me: GuildMember, channel: Channel | GuildChannel | TextBasedChannels) {
 		if (!channel.isText()) return false;
 		if (channel.isThread()) return channel.sendable;
