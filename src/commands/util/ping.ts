1d5d60e1d50900b2b2d35132ac1493a74a723660
hellobafu <brage.spammail@gmail.com>
2021-09-09 21:15:54 +0200

    fix: description

--- src/commands/util/ping.ts
+++ src/commands/util/ping.ts
@@ -1,10 +1,9 @@
 import type { ApplicationCommandData } from "discord.js";
 import type { CmdIntr } from "../../Typings.js";
 
-export const defaultHide = false;
 export const data: ApplicationCommandData = {
 	name: "ping",
-	description: "Check that the bot is running"
+	description: "Check if the bot is running"
 };
 
 export async function execute(intr: CmdIntr) {
