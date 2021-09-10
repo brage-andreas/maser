b689a0a4439690a8b78cfb37c98858b31d1a7227
hellobafu <brage.spammail@gmail.com>
2021-09-10 23:51:15 +0200

    remove: temporary logChannel function

--- src/database/logChannel.ts
+++
@@ -1,6 +0,0 @@
-import type { Guild } from "discord.js";
-
-// temporary
-export async function getLogChannel(guild: Guild) {
-	return guild.channels.cache.find((ch) => ch.name === "bot-log") ?? null;
-}
