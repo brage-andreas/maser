bd30512dfd8ede250ab00d230cd35fcf167566c2
hellobafu <brage.spammail@gmail.com>
2021-09-11 19:16:05 +0200

    feat: a lot of changes

--- src/database/ConfigManager.ts
+++ src/database/ConfigManager.ts
@@ -1,74 +1,63 @@
-import type { Guild, TextChannel } from "discord.js";
+import type { TextChannel } from "discord.js";
+import { Clint } from "../extensions/Clint.js";
 import { ConfigColumns, ConfigResult } from "../typings.js";
-import Postgres from "./src/connection.js";
+import Postgres, { CreatorOptions } from "./src/postgres.js";
 
 // TODO: refactor
 
 export class ConfigManager extends Postgres {
+	constructor(client: Clint, options?: CreatorOptions) {
+		super(client, options);
+		this.setSchema("configs");
+	}
+
 	public botLogChannel = {
-		get: async (guildResolvable: Guild | string): Promise<TextChannel | null> => {
-			return this.getChannel(guildResolvable, "bot_log_channel_id");
+		get: async (): Promise<TextChannel | null> => {
+			this.setTable("logs");
+			return this.getChannel("bot_log_channel_id");
 		},
 
-		set: (newChannel: TextChannel | string, guildResolvable: Guild | string): Promise<boolean> => {
-			return this.setChannel(newChannel, guildResolvable, "bot_log_channel_id");
+		set: (newChannel: TextChannel | string): Promise<boolean> => {
+			this.setTable("logs");
+			return this.setChannel(newChannel, "bot_log_channel_id");
 		}
 	};
 
 	public memberLogChannel = {
-		get: async (guildResolvable: Guild | string): Promise<TextChannel | null> => {
-			return this.getChannel(guildResolvable, "member_log_channel_id");
+		get: async (): Promise<TextChannel | null> => {
+			this.setTable("logs");
+			return this.getChannel("member_log_channel_id");
 		},
 
-		set: (newChannel: TextChannel | string, guildResolvable: Guild | string): Promise<boolean> => {
-			return this.setChannel(newChannel, guildResolvable, "member_log_channel_id");
+		set: (newChannel: TextChannel | string): Promise<boolean> => {
+			this.setTable("logs");
+			return this.setChannel(newChannel, "member_log_channel_id");
 		}
 	};
 
-	private async one(query: string): Promise<ConfigResult | null> {
-		return this.psql.one(query).catch(() => null);
-	}
-
-	private async none(query: string): Promise<boolean> {
-		return this.psql
-			.none(query)
-			.then(() => true)
-			.catch(() => false);
-	}
-
-	private async getChannel(guildResolvable: Guild | string, key: ConfigColumns): Promise<TextChannel | null> {
-		const guildId = typeof guildResolvable === "string" ? guildResolvable : guildResolvable.id;
+	private async getChannel(key: ConfigColumns): Promise<TextChannel | null> {
+		if (!this.guildId) throw new Error("Guild id must be set to the ConfigManager");
 
 		const query = `
                 SELECT ${key}
                 FROM configs.logs
-                WHERE id = ${guildId}
+                WHERE id = ${this.guildId}
             `;
 
-		const channelId = await this.one(query).then((result) => result?.[key] ?? null);
+		const channelId = await this.one<ConfigResult>(query).then((result) => result?.[key] ?? null);
 		if (!channelId) return null;
 
-		const guild = this.client.guilds.cache.get(guildId);
+		const guild = this.client.guilds.cache.get(this.guildId);
 		const channel = guild?.channels.cache.get(channelId);
 		if (!channel || channel.type !== "GUILD_TEXT") return null;
 
 		return channel as TextChannel;
 	}
 
-	private async setChannel(
-		newChannel: TextChannel | string,
-		guildResolvable: Guild | string,
-		key: ConfigColumns
-	): Promise<boolean> {
+	private async setChannel(newChannel: TextChannel | string, key: ConfigColumns): Promise<boolean> {
 		const channelId = typeof newChannel === "string" ? newChannel : newChannel.id;
-		const guildId = typeof guildResolvable === "string" ? guildResolvable : guildResolvable.id;
 
-		const query = `
-                UPDATE configs.logs
-                SET ${key}=${channelId}
-                WHERE id = ${guildId};
-            `;
-
-		return this.none(query);
+		this.setGuild(this.guildId).setSchema("configs");
+		return this.updateRow("logs", [key], [channelId]);
 	}
 }
