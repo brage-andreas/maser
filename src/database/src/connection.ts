37a2575b773f036d547244e95bd256c4b521ab2a
hellobafu <brage.spammail@gmail.com>
2021-09-11 10:00:50 +0200

    refactor: imports

--- src/database/src/connection.ts
+++ src/database/src/connection.ts
@@ -1,5 +1,5 @@
+import { Clint } from "../../extensions/Clint.js";
 import postgres from "pg-promise";
-import { Clint } from "../../extensions/Clint";
 
 const connectionString = "postgres://postgres:admin@localhost:5432/maser";
 const psql = postgres()(connectionString);
