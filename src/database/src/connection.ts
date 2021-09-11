b91bb32dce598e72fc2be0699dac75cb1909d241
hellobafu <brage.spammail@gmail.com>
2021-09-11 19:16:35 +0200

    feat: add connection shorthands

--- src/database/src/connection.ts
+++ src/database/src/connection.ts
@@ -2,13 +2,32 @@ import { Clint } from "../../extensions/Clint.js";
 import postgres from "pg-promise";
 
 const connectionString = "postgres://postgres:admin@localhost:5432/maser";
-const psql = postgres()(connectionString);
+const connection = postgres()(connectionString);
 
-export default class Postgres {
+export default class PostgresConnection {
 	public client: Clint;
-	public psql = psql;
+	public connection = connection;
 
 	constructor(client: Clint) {
 		this.client = client;
+	}
+
+	// TODO: this generic could probably be better
+	// TODO: properly type everything this thing can return
+	public async one<T>(query: string): Promise<T | null> {
+		return this.connection.one(query);
+	}
+
+	public async none(query: string): Promise<boolean> {
+		try {
+			await this.connection.none(query);
+			return true;
+		} catch {
+			return false;
+		}
+	}
+
+	public async oneOrNone<T>(query: string): Promise<T | null> {
+		return this.connection.oneOrNone(query);
 	}
 }
