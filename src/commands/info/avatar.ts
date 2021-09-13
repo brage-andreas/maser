2bc1e2988007abbe11ee1d9f093136a9d742b8c6
hellobafu <brage.spammail@gmail.com>
2021-09-13 23:22:08 +0200

    fix: pushed too early

--- src/commands/info/avatar.ts
+++ src/commands/info/avatar.ts
@@ -1,6 +1,7 @@
 import type { CmdIntr } from "../../typings.js";
-import { ApplicationCommandData, GuildMember, MessageEmbed } from "discord.js";
+import type { AllowedImageSize, ApplicationCommandData, GuildMember } from "discord.js";
 import { ApplicationCommandOptionType } from "discord-api-types/v9";
+import { MessageEmbed } from "discord.js";
 
 export const data: ApplicationCommandData = {
 	name: "avatar",
@@ -25,23 +26,23 @@ type ImageFormats = "webp" | "png" | "jpg";
 export async function execute(intr: CmdIntr) {
 	const user = intr.options.getUser("user") ?? intr.user;
 	const member = intr.options.getMember("user") as GuildMember | null;
+	const size = (intr.options.getInteger("size") ?? 2048) as AllowedImageSize;
 
 	const base = user.displayAvatarURL().split(".").slice(0, -1).join(".");
-	const formats: ImageFormats[] = ["webp", "png", "jpg"];
 
-	const getURL = (format: ImageFormats) => `${base}.${format}?size=2048`;
+	const getURL = (format: ImageFormats) => `${base}.${format}?size=${size}`;
 
 	const webp = getURL("webp");
 	const png = getURL("png");
 	const jpg = getURL("jpg");
-	const dynamic = user.displayAvatarURL({ size: 2048, dynamic: true });
+	const dynamic = user.displayAvatarURL({ size, dynamic: true });
 
 	const name = member && !member.pending ? member.displayName : user.username;
 	const nameStr = name.endsWith("s") || name.endsWith("z") ? `${name}' avatar` : `${name}'s avatar`;
 
 	const description: string[] = [];
 
-	const isGIF = dynamic.endsWith(".gif?size=2048");
+	const isGIF = dynamic.endsWith(`.gif?size=${size}`);
 	if (isGIF) description.push(`[gif](${dynamic})`);
 
 	description.push(`[png](${png}) [jpg](${jpg}) [webp](${webp})`);
