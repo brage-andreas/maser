8e7cd185cd74ec58afce775486a74688b46277d1
hellobafu <brage.spammail@gmail.com>
2021-10-02 08:56:03 +0200

    feat: support for guild avatars

--- src/commands/info/avatar.ts
+++ src/commands/info/avatar.ts
@@ -1,5 +1,5 @@
-import type { CmdIntr } from "../../typings.js";
 import type { AllowedImageSize, ApplicationCommandData, GuildMember } from "discord.js";
+import type { CmdIntr } from "../../typings.js";
 import { ApplicationCommandOptionType } from "discord-api-types/v9";
 import { MessageEmbed } from "discord.js";
 
@@ -9,14 +9,22 @@ export const data: ApplicationCommandData = {
 	options: [
 		{
 			name: "user",
-			description: "The user to target",
+			description: "The user to target. Default is you",
 			type: ApplicationCommandOptionType.User as number
 		},
 		{
 			name: "size",
-			description: "Size of the image. 2048px is default",
+			description: "Size of the image. Default is 2048px",
 			type: ApplicationCommandOptionType.Integer as number,
-			choices: Array.apply(null, Array(9)).map((_, i) => ({ name: `${2 ** (i + 4)}px`, value: 2 ** (i + 4) }))
+			choices: Array.apply(null, Array(9)).map((_, i) => ({
+				name: `${2 ** (i + 4)}px`,
+				value: 2 ** (i + 4)
+			}))
+		},
+		{
+			name: "guild-avatar",
+			description: "Include guild avatars. Default is true",
+			type: ApplicationCommandOptionType.Boolean as number
 		}
 	]
 };
@@ -24,18 +32,22 @@ export const data: ApplicationCommandData = {
 type ImageFormats = "webp" | "png" | "jpg";
 
 export async function execute(intr: CmdIntr) {
-	const user = intr.options.getUser("user") ?? intr.user;
+	const includeGuildAvatar = intr.options.getBoolean("guild-avatar") ?? true;
 	const member = intr.options.getMember("user") as GuildMember | null;
 	const size = (intr.options.getInteger("size") ?? 2048) as AllowedImageSize;
+	const user = intr.options.getUser("user") ?? intr.user;
 
-	const base = user.displayAvatarURL().split(".").slice(0, -1).join(".");
+	const target = includeGuildAvatar && member ? member : user;
+
+	const baseURL = target.displayAvatarURL();
+	const base = baseURL.split(".").slice(0, -1).join(".");
 
 	const getURL = (format: ImageFormats) => `${base}.${format}?size=${size}`;
 
 	const webp = getURL("webp");
 	const png = getURL("png");
 	const jpg = getURL("jpg");
-	const dynamic = user.displayAvatarURL({ size, dynamic: true });
+	const dynamic = target.displayAvatarURL({ size, dynamic: true });
 
 	const name = member && !member.pending ? member.displayName : user.username;
 	const nameStr = name.endsWith("s") || name.endsWith("z") ? `${name}' avatar` : `${name}'s avatar`;
