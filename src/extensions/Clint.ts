a16592a4bbe701a003b9f5f60e28c5dc6108cf71
hellobafu <brage.spammail@gmail.com>
2021-10-02 14:12:16 +0200

    feat: use makeCache

--- src/extensions/Clint.ts
+++ src/extensions/Clint.ts
@@ -1,4 +1,4 @@
-import type { ClientOptions } from "discord.js";
+import { ClientOptions, Message, Options } from "discord.js";
 
 import { CommandManager, EventManager, ColorManager, EmojiManager } from "./";
 import { InfoLogger } from "../utils/logger/";
@@ -7,23 +7,38 @@ import { Client } from "discord.js";
 
 export default class Clint extends Client<true> {
 	commands: CommandManager;
-	colors: ColorManager;
 	events: EventManager;
+	colors: ColorManager;
 	logger: InfoLogger;
 	moji: EmojiManager;
 
 	constructor(options?: ClientOptions) {
+		const defaultCacheSettings = {
+			...Options.defaultMakeCacheSettings,
+			MessageManager: {
+				sweepInterval: 120, // 2 min
+				sweepFilter: () => (msg: Message) => {
+					const age = msg.editedTimestamp ?? msg.createdTimestamp;
+					const fromBot = msg.author.bot;
+					const now = Date.now();
+					return fromBot || now - age > 900_000; // 15 min
+				}
+			}
+		};
+
 		const defaultOptions: ClientOptions = {
 			allowedMentions: { repliedUser: false },
-			intents: INTENTS
+			failIfNotExists: false,
+			intents: INTENTS,
+			makeCache: Options.cacheWithLimits(defaultCacheSettings)
 		};
 
 		super(options ?? defaultOptions);
 
 		this.commands = new CommandManager();
-		this.logger = new InfoLogger();
 		this.events = new EventManager(this);
 		this.colors = new ColorManager();
+		this.logger = new InfoLogger();
 		this.moji = new EmojiManager(this);
 	}
 }
