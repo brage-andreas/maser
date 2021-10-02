b4228c5435d4c5d3d40b93e4b1eb72518a9a3533
hellobafu <brage.spammail@gmail.com>
2021-10-02 08:56:54 +0200

    feat: safer checks

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -3,7 +3,7 @@ import type { Clint } from "../extensions/";
 import { CommandLogger } from "../utils/logger/";
 
 export async function execute(client: Clint, intr: CmdIntr) {
-	if (!intr.isCommand()) return;
+	if (!intr.isCommand() || !intr.guild || !intr.member) return;
 
 	const ephemeral = client.commands.getDefaultHide(intr);
 	await intr.deferReply({ ephemeral });
