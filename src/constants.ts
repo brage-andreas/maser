06c4a2405c7bc1140aac1e540722b199208af156
hellobafu <brage.spammail@gmail.com>
2021-10-03 22:59:13 +0200

    refactor: cleanup

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,16 @@
 import { Intents } from "discord.js";
 
-export const INTENTS = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES];
+export const INTENTS = [
+	Intents.FLAGS.GUILDS, //
+	Intents.FLAGS.GUILD_MEMBERS,
+	Intents.FLAGS.GUILD_MESSAGES
+];
 
 // * REGEXP
-export const GUILDEMOJI_REGEX = /<?(a)?:?(\w{2,32}):(\d{17,19})>?/;
+// export const GUILDEMOJI_REGEX = /<?(a)?:?(\w{2,32}):(\d{17,19})>?/;
 export const CODEBLOCK_REGEX = /```(?:(?<lang>\S+)\n)?\s?(?<code>[^]+?)\s?```/;
-export const INVITE_REGEX = /(?:https?:\/\/)?(?:www\.)?discord(?:\.gg|(?:app)?\.com\/invite)\/(\S+)/;
-export const TOKEN_REGEX = /[\w-]{24}\.[\w-]{6}\.[\w-]{27}/;
+// export const INVITE_REGEX = /(?:https?:\/\/)?(?:www\.)?discord(?:\.gg|(?:app)?\.com\/invite)\/(\S+)/;
+// export const TOKEN_REGEX = /[\w-]{24}\.[\w-]{6}\.[\w-]{27}/;
 export const CODE_REGEX = /`(?<code>.+?)`/;
 
 export const CHANNEL_REGEX = /^<#\d{17,19}>$/;
