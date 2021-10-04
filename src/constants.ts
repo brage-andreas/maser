b5c21addca5be62b6d30856aa20b05a14e785d4d
hellobafu <brage.spammail@gmail.com>
2021-10-04 13:13:41 +0200

    fix: uncomment used constant

--- src/constants.ts
+++ src/constants.ts
@@ -10,7 +10,7 @@ export const INTENTS = [
 // export const GUILDEMOJI_REGEX = /<?(a)?:?(\w{2,32}):(\d{17,19})>?/;
 export const CODEBLOCK_REGEX = /```(?:(?<lang>\S+)\n)?\s?(?<code>[^]+?)\s?```/;
 // export const INVITE_REGEX = /(?:https?:\/\/)?(?:www\.)?discord(?:\.gg|(?:app)?\.com\/invite)\/(\S+)/;
-// export const TOKEN_REGEX = /[\w-]{24}\.[\w-]{6}\.[\w-]{27}/;
+export const TOKEN_REGEX = /[\w-]{24}\.[\w-]{6}\.[\w-]{27}/;
 export const CODE_REGEX = /`(?<code>.+?)`/;
 
 export const CHANNEL_REGEX = /^<#\d{17,19}>$/;
