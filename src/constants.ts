f0521fdab7ed0c61cdbac15d0291cce9f00e996b
hellobafu <brage.spammail@gmail.com>
2021-10-04 19:53:13 +0200

    refactor: add comment

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import { Intents } from "discord.js";
 export const INTENTS = [
 	Intents.FLAGS.GUILDS, //
 	Intents.FLAGS.GUILD_MEMBERS,
-	Intents.FLAGS.GUILD_MESSAGES
+	Intents.FLAGS.GUILD_MESSAGES // temporary
 ];
 
 // * REGEXP
