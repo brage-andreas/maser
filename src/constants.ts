0e8af5f2f876226a7cd387c4f32127de6e0c785d
hellobafu <brage.spammail@gmail.com>
2021-10-05 10:51:16 +0200

    feat: move maxx embed description len and refactor

--- src/constants.ts
+++ src/constants.ts
@@ -6,21 +6,18 @@ export const INTENTS = [
 	Intents.FLAGS.GUILD_MESSAGES // temporary
 ];
 
-// * REGEXP
+export const MAX_EMBED_DESCRIPTION_LEN = 4096;
+
 // export const GUILDEMOJI_REGEX = /<?(a)?:?(\w{2,32}):(\d{17,19})>?/;
-export const CODEBLOCK_REGEX = /```(?:(?<lang>\S+)\n)?\s?(?<code>[^]+?)\s?```/;
 // export const INVITE_REGEX = /(?:https?:\/\/)?(?:www\.)?discord(?:\.gg|(?:app)?\.com\/invite)\/(\S+)/;
+export const CODEBLOCK_REGEX = /```(?:(?<lang>\S+)\n)?\s?(?<code>[^]+?)\s?```/;
+export const CHANNEL_REGEX = /^<#\d{17,19}>$/;
 export const TOKEN_REGEX = /[\w-]{24}\.[\w-]{6}\.[\w-]{27}/;
 export const CODE_REGEX = /`(?<code>.+?)`/;
-
-export const CHANNEL_REGEX = /^<#\d{17,19}>$/;
 export const ROLE_REGEX = /^<@&\d{17,19}>$/;
 export const USER_REGEX = /^<@!?\d{17,19}>$/;
-
 export const ID_REGEX = /^\d{17,19}$/;
-// *
 
-// * COLORS
 export const BLURPLE = "5865F2";
 export const ORANGE = "FF8741";
 export const YELLOW = "FFC152";
@@ -29,11 +26,8 @@ export const GREEN = "5AD658";
 export const INVIS = "2F3136";
 export const WHITE = "FFFFFF";
 export const RED = "FF5733";
-
 export const COLORS = { BLACK, BLURPLE, ORANGE, YELLOW, GREEN, INVIS, WHITE, RED };
-// *
 
-// * FLAGS
 export const USER_FLAGS = {
 	EARLY_VERIFIED_BOT_DEVELOPER: "early developer",
 	DISCORD_CERTIFIED_MODERATOR: "certified mod",
@@ -49,7 +43,6 @@ export const USER_FLAGS = {
 	VERIFIED_BOT: "verified bot",
 	TEAM_USER: "team user"
 };
-// *
 
 export enum LOGGER_TYPES {
 	COMMAND,
@@ -64,9 +57,9 @@ export enum CONFIG_OPTIONS {
 	ROLES
 }
 
-export const BOOST_LEVELS = {
-	TIER_3: "level 3",
-	TIER_2: "level 2",
-	TIER_1: "level 1",
-	NONE: "no level"
-};
+export enum BOOST_LEVELS {
+	TIER_3 = "level 3",
+	TIER_2 = "level 2",
+	TIER_1 = "level 1",
+	NONE = "no level"
+}
