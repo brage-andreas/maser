73dee04241b642566a724da5fa74dbe93447940a
hellobafu <brage.spammail@gmail.com>
2021-10-05 23:32:07 +0200

    refactor: remove channel count log

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,13 +2,12 @@ import type { Client } from "../extensions/";
 
 export async function execute(client: Client) {
 	const guilds = client.guilds.cache.size;
-	const channels = client.channels.cache.size;
 	const users = client.guilds.cache.reduce((count, guild) => count + guild.memberCount, 0);
 
 	client.logger.log(
-		`Logged on as ${client.user!.tag} (${client.user!.id})`,
-		`In ${guilds} guilds, ${channels} channels, and ${users} users`
+		`Logged on as ${client.user.tag} (${client.user.id})`,
+		`In ${guilds} guilds with ${users} total members`
 	);
 
-	if (!client.application.owner) await client.application?.fetch();
+	if (!client.application.owner) await client.application.fetch();
 }
