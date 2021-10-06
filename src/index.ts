bdc6c02729f994a87267e75545aa8fa079fba406
hellobafu <brage.spammail@gmail.com>
2021-10-06 08:59:42 +0200

    refactor: remove comments

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,3 @@ await client.commands.init();
 await client.events.init();
 
 client.login(process.env.TOKEN);
-
-// TODO: add enable/disable commands command
-// TODO: permissions checks for sending embeds in log channels
