83ea1e3290a4c8928d8988844760e4849d587517
hellobafu <brage.spammail@gmail.com>
2021-10-25 10:33:48 +0200

    feat: ability to create mute role

--- src/commands/mod/mute.ts
+++ src/commands/mod/mute.ts
@@ -1,5 +1,6 @@
 import type { ChatInputApplicationCommandData } from "discord.js";
 import type { CommandInteraction, Command } from "../../typings.js";
+
 import { ApplicationCommandOptionTypes } from "discord.js/typings/enums";
 import ConfigManager from "../../database/src/config/ConfigManager.js";
 import { ConfirmationButtons } from "../../extensions/ButtonManager.js";
@@ -7,6 +8,37 @@ import { ConfirmationButtons } from "../../extensions/ButtonManager.js";
 const options = {
 	wip: true
 };
+
+const FIFTEEN_MIN = 900000;
+const FOURTY_FIVE_MIN = 2700000;
+const ONE_AND_HALF_HRS = 129600000;
+const THREE_HRS = 10800000;
+const SIX_HRS = 21600000;
+const TWELVE_HRS = 44200000;
+const ONE_DAY = 86400000;
+const THREE_DAYS = 259200000;
+
+const getDefaultMuteRoleData = (intr: CommandInteraction) => ({
+	reason: `Automatic mute role created by ${intr.user.tag} (${intr.user.id})`,
+	mentionable: false,
+	name: "Muted",
+	hoist: false,
+	position: 1,
+	permissions: intr.guild.roles.everyone.permissions.remove(
+		"SEND_MESSAGES_IN_THREADS",
+		"USE_APPLICATION_COMMANDS",
+		"CREATE_PRIVATE_THREADS",
+		"CREATE_PUBLIC_THREADS",
+		"MENTION_EVERYONE",
+		"ADD_REACTIONS",
+		"SEND_MESSAGES",
+		"ATTACH_FILES",
+		"EMBED_LINKS",
+		"CONNECT",
+		"USE_VAD",
+		"SPEAK"
+	)
+});
 
 const data: ChatInputApplicationCommandData = {
 	name: "mute",
@@ -25,28 +57,35 @@ const data: ChatInputApplicationCommandData = {
 		},
 		{
 			name: "duration",
-			type: ApplicationCommandOptionTypes.STRING,
+			type: ApplicationCommandOptionTypes.INTEGER,
 			description: "The duration for this mute",
 			choices: [
-				{ name: "3 hours (default)", value: "3h" },
-				{ name: "15 minutes", value: "15m" },
-				{ name: "45 minutes", value: "45m" },
-				{ name: "1,5 hours", value: "1.5h" },
-				{ name: "6 hours", value: "6h" },
-				{ name: "12 hours", value: "12h" },
-				{ name: "1 day", value: "1d" },
-				{ name: "3 days", value: "3d" }
+				{ name: "3 hours (default)", value: THREE_HRS },
+				{ name: "15 minutes", value: FIFTEEN_MIN },
+				{ name: "45 minutes", value: FOURTY_FIVE_MIN },
+				{ name: "1,5 hours", value: ONE_AND_HALF_HRS },
+				{ name: "6 hours", value: SIX_HRS },
+				{ name: "12 hours", value: TWELVE_HRS },
+				{ name: "1 day", value: ONE_DAY },
+				{ name: "3 days", value: THREE_DAYS }
 			]
 		}
 	]
 };
 
+// TODO: refactor
 async function execute(intr: CommandInteraction) {
 	const target = intr.options.getMember("user", true);
 	const reason = intr.options.getString("reason") ?? "No reason provided";
-	const duration = intr.options.getString("duration") ?? "3h";
+	const duration = intr.options.getInteger("duration") ?? THREE_HRS;
+	const endTimestamp = Date.now() + duration;
 
-	const [idEm, atEm, errEm] = intr.client.systemEmojis.findAndParse("id_red", "at", "exclamation");
+	const [idEm, atEm, errEm, sucEm] = intr.client.systemEmojis.findAndParse("id_red", "at", "exclamation", "success");
+
+	if (!intr.guild.me?.permissions.has("MANAGE_ROLES")) {
+		intr.editReply(`${errEm}I don't have permissions to add or remove roles`);
+		return;
+	}
 
 	if (!target) {
 		intr.editReply(`${idEm}The user to target was not found in this server`);
@@ -94,11 +133,52 @@ async function execute(intr: CommandInteraction) {
 					config
 						.set(existingMuteRole.id)
 						.then(() => {
-							intr.editReply({ content: "Done! You're good to go now.", components: [] });
+							intr.editReply({ content: `${sucEm}Done! You're good to go now.`, components: [] });
 						})
 						.catch(() => {
 							intr.editReply({
-								content: "Something went wrong with setting your mute role.",
+								content: `${errEm}Something went wrong with setting your mute role.`,
+								components: []
+							});
+						});
+				})
+				.catch(() => {
+					intr.editReply("Gotcha. Command canceled");
+				});
+		} else {
+			const query = `${atEm}You have not set a mute role in your config. Do you want to me to create one?\nYou can edit it afterwards.`;
+			const collector = new ConfirmationButtons({ author: intr.user })
+				.setInteraction(intr)
+				.setUser(intr.user)
+				.setQuery(query);
+
+			collector
+				.start({ noReply: true })
+				.then(async () => {
+					intr.guild.roles
+						.create(getDefaultMuteRoleData(intr))
+						.then((newMutedRole) => {
+							const success = `${sucEm}Done! Created ${newMutedRole} and set it to your config. You're good to go now.`;
+							const fail = `${errEm}Created ${newMutedRole} and set it to your config, but omething went wrong with setting your mute role.`;
+							config
+								.set(newMutedRole.id)
+								.then(() => {
+									intr.editReply({
+										content: success,
+										allowedMentions: { parse: [] },
+										components: []
+									});
+								})
+								.catch(() => {
+									intr.editReply({
+										content: fail,
+										components: []
+									});
+								});
+						})
+						.catch((reason) => {
+							intr.editReply({
+								content: `${errEm}Creating the role failed with reason: ${reason}`,
 								components: []
 							});
 						});
@@ -107,6 +187,8 @@ async function execute(intr: CommandInteraction) {
 					intr.editReply("Gotcha. Command canceled");
 				});
 		}
+	} else {
+		intr.editReply("omg muted");
 	}
 
 	intr.logger.log(`Muted`);
