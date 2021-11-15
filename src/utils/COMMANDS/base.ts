4aee7d2e3f7dd53b7365cae2e2062c8924d367ce
Drango <37003366+el-durango@users.noreply.github.com>
2021-11-15 10:07:27 +0100

    refactor: id -> ID

--- src/utils/COMMANDS/base.ts
+++ src/utils/COMMANDS/base.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const clientId = process.env.CLIENT_ID;
 const guildId = process.env.GUILD_ID;
 
-if (!clientId) throw new Error("Client id must be set in .env file");
+if (!clientId) throw new Error("Client ID must be set in .env file");
 
 const manager = new CommandHandler();
 await manager.init();
