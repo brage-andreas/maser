3657cb2c451102ba72d0c80209ac5708c1fcd335
Drango <brage.spammail@gmail.com>
2021-12-12 14:29:09 +0100

    refactor: remove unused import

--- src/typings/discord.js.d.ts
+++ src/typings/discord.js.d.ts
@@ -3,7 +3,6 @@ import "discord.js";
 import type { CommandHandler, EventManager, CommandManager } from "../modules/index.js";
 import type { InfoLogger, CommandLogger } from "../utils/logger/index.js";
 import type { EMOJIS, COLORS } from "../constants.js";
-import type { ClientOptions } from "discord.js";
 
 declare module "discord.js" {
 	interface Client {
