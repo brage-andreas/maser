58585b65e4eeeb29b784c9585086ddf83d86542d
Drango <draaangooo@gmail.com>
2022-05-15 14:40:30 +0200

    refactor: remove unused emojis

--- src/constants/emojis.ts
+++ src/constants/emojis.ts
@@ -1,16 +1,8 @@
-export type MaserEmojis =
-	| "check"
-	| "cross"
-	| "lock"
-	| "sparkles"
-	| "warning"
-	| "wip";
+export type MaserEmojis = "check" | "cross" | "lock" | "warning" | "wip";
 
-// Source of emojis are located in /icons
-export const EMOJIS: Record<MaserEmojis | "boost", string> = {
-	sparkles: "<:maser_sparkles:924421206143295517>",
+// Source in root/icons
+export const EMOJIS: Record<MaserEmojis, string> = {
 	warning: "<:maser_warning:924421206105546822>",
-	boost: "<:discord_booster:938523381580595273>",
 	check: "<:maser_check:924421206122328114>",
 	cross: "<:maser_cross:924421206017474630>",
 	lock: "<:maser_lock:924421206046814208>",
